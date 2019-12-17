08c1d423383fc4626eef62da51aff65047f58eb3
Bryan Szendel <bryan.szendel@gmail.com>
2019-12-17 10:24:27 -0700

    refactored Stack push pop and len

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -8,24 +8,15 @@ class Stack:
         self.storage = DoublyLinkedList()
 
     def push(self, value):
-        self.value = value
-        if self.storage.__len__() == 0:
-            self.storage.add_to_tail(self.value)
-        else:
-            self.storage.add_to_tail(value)
-            return self.storage.tail.value
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        if self.storage.__len__() == 0:
+        if self.size == 0:
             return None
-        elif self.storage.__len__() == 1:
-            tailVal = self.storage.tail.value
-            self.storage.remove_from_head()
-            return tailVal
         else:
-            tailVal = self.storage.tail.value
-            self.storage.remove_from_tail()
-            return tailVal
+            self.size -= 1
+            return self.storage.remove_from_tail()
 
     def len(self):
-        return self.storage.__len__()
+        return self.size
