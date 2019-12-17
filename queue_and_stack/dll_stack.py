0de883ecc721c3b09ce57f6f0b3dfbf547f9fdaa
Bryan Szendel <bryan.szendel@gmail.com>
2019-12-16 17:04:41 -0700

    push pop and len return appropriate values

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,13 +6,38 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.value = value
+        if self.storage.__len__() == 0:
+            self.storage.add_to_tail(self.value)
+            # self.storage.add_to_tail(value)
+        # return self.storage.tail.value
+        # elif self.storage.__len__() is None: 
+        #     return None
+        else:
+            self.storage.add_to_tail(value)
+            return self.storage.tail.value
 
     def pop(self):
-        pass
+        # print('STORAGE', self.storage.__len__())
+        if self.storage.__len__() == 0:
+            return None
+        elif self.storage.__len__() == 1:
+            tailVal = self.storage.tail.value
+            self.storage.remove_from_head()
+            return tailVal
+        # elif self.storage.__len__() is None:
+        #     return None
+        # else:
+            # returnVal = self.storage.tail.value
+        else:
+            tailVal = self.storage.tail.value
+            self.storage.remove_from_tail()
+            return tailVal
+        # return self.storage.tail.value
 
     def len(self):
-        pass
+        # print(self.storage.__len__())
+        return self.storage.__len__()
